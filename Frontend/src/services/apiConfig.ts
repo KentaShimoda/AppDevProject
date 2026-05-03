5885c26b745c91e41fad7cc0dc737ac1ba6e4b94
Kenta Shimoda <202411251@gordoncollege.edu.ph>
2026-05-04 02:05:15 +0800

    api way fixed

--- Frontend/src/services/apiConfig.ts
+++ Frontend/src/services/apiConfig.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5016/api";
+export const API_BASE_URL = "https://appdevproject-8ffe.onrender.com/api";
 
 export const getAuthHeaders = (isMultipart = false) => {
   const token = localStorage.getItem("token");
