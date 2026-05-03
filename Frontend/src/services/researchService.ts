35e58f0c78ebb514f6564e90f8b1494fed4e09e6
Kenta Shimoda <202411251@gordoncollege.edu.ph>
2026-05-03 21:54:20 +0800

    fixed the api error

--- Frontend/src/services/researchService.ts
+++ Frontend/src/services/researchService.ts
@@ -1,5 +1,5 @@
 // src/services/researchService.ts
-const API_BASE_URL = "http://localhost:5016/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const researchService = {
   getAll: async () => {
