79db388c5ee38a47c3f097b72d4f19f6d6cbba47
Kenta Shimoda <202411251@gordoncollege.edu.ph>
2026-05-03 20:47:51 +0800

    added dependencies and package

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d0644d1454c44038a3d41c3c0224c5f3d6d1d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b82d6f67adff3a86f46c204491d7fe5a695462c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
